c3c0da7d5a368c0e3bae123eeb4d251f6c04159e
Vlad Nestorov <vlad@nestorov.net>
2022-01-27 08:57:30 -0500

    fix unary example, add test cases

--- ch3.test.js
+++ ch3.test.js
@@ -1,8 +1,31 @@
 const unary = require('./index.js');
 
 it('should pass only a single argument', function () {
-    const mockFn = jest.fn();
+    const mockFn = jest.fn().mockReturnValue('mockResult');
+
     const unaryFn = unary(mockFn);
-    unaryFn(1, "2", [3]);
+
+    expect(
+        unaryFn(1, "2", [3])
+    ).toBe(
+        'mockResult'
+    )
     expect(mockFn).lastCalledWith(1);
-}); 
+});
+
+
+it('using parseInt in map directly leads to unexpected results', function () {
+    expect(
+        ["1", "2", "3"].map(parseInt)
+    ).toEqual(
+        [1, Number.NaN, Number.NaN]
+    );
+});
+
+it('changing behavior of parseInt by passing single argument leads to expected results', function () {
+    expect(
+        ["1", "2", "3"].map( unary(parseInt) )
+    ).toEqual(
+        [1, 2, 3]
+    );
+});
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function unary(fn) {
     return function firstArgument( arg ) {
-        fn( arg );
+        return fn( arg );
     }
 }
 
