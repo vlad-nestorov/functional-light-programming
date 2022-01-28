c0e5033a913c515e1ea4f6e01137271ec9f2287c
Vlad Nestorov <vlad@nestorov.net>
2022-01-27 20:59:33 -0500

    play around with curry

--- ch3.test.js
+++ ch3.test.js
@@ -29,3 +29,50 @@ it('changing behavior of parseInt by passing single argument leads to expected r
         [1, 2, 3]
     );
 });
+
+it('should apply partial ', function () {
+    function partial(fn, ...args) {
+        return function partialFn(...remainingArgs) {
+            return fn(...args, ...remainingArgs)
+        }
+    }
+
+    const mockFn = jest.fn();
+
+    const partialFn = partial(mockFn, "Hello");
+
+    partialFn("World");
+
+    expect(mockFn).lastCalledWith("Hello", "World");
+
+});
+
+it('should curry a function', function () {
+    function partial(fn, ...args) {
+        return function partialFn(...remainingArgs) {
+            return fn(...args, ...remainingArgs)
+        }
+    }
+
+    function curry(fn, arity = fn.length) {
+        return arity < 2 ? fn :
+            function partialCurry( arg ) {
+                return curry( partial(fn, arg), --arity  );
+            }
+    }
+
+    function concat(x, y, z) {
+        return x + y + z;
+    }
+
+
+    const curriedFn = curry( concat, 3 );
+
+
+    expect(
+        curriedFn("hello")("world")("here's")
+    ).toEqual("helloworldhere's")
+
+
+
+});
